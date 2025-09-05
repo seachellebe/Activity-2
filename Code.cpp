4626dbf2b1569aee558b6bdb582f6b739fe95085
Michelle <225957291+seachellebe@users.noreply.github.com>
2025-09-05 19:47:02 +0800

    Update Code.cpp

--- Code.cpp
+++ Code.cpp
@@ -1,18 +1,26 @@
 #include <iostream>
+#include <string>
 using namespace std;
 int main()
 {
-    int hours, minutes;
+    string full_name, course_year_section, birthday, motto;
     
-    cout << "Expected Output: " << "\n";
-    cout << "Input hours: ";
-    cin >> hours;
+    cout << "Enter your Full пame: ";
+    getline (cin, full_name);
     
-    cout << "Input minutes: ";
-    cin >> minutes;
+    cout << "Enter your Course, Yr. & Sec.: ";
+    getline (cin, course_year_section);
     
-    int total_minutes = hours * 60;
-    cout << "Total: " << total_minutes << "minutes" << endl;
+    cout << "Enter your Birthday: ";
+    getline (cin, birthday);
+    
+    cout << "Enter your Motto/Motivation in Life: ";
+    getline (cin, motto);
+    
+    cout << endl;
+    cout << "Hi! I'm " << full_name << " of " << course_year_section << ". Welcome to Data Structures and Algorithms" << endl;
+    cout << "My Birthday is on " << birthday << ". And my motto/motivation in life is " << motto << endl;
     
     return 0;
-}
+    
+} 
